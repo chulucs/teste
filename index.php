07b04886879707c4736714ba315d6be630425939
chulucs <fabiocsegre@gmail.com>
2018-06-20 16:28:26 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+	<meta charset="utf-8">
+<!--css -->
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+
+       <title></title>
+
+   </head>
+   <body>
+       <div class="container">
+
+       </div>
+   </body>
+</html>
